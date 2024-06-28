2e58051d6ea04e2974ed31c613b1773df71152d9
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-28 16:19:28 +0200

    added logic to complete the challenge

---
+++ index.js
@@ -0,0 +1,10 @@
+// Chapter 3 Functions 
+// Exercise 1 Minimum
+
+function minimumValue (a, b) {
+    if (a < b ) return a;
+    else return b;
+}
+
+console.log(minimumValue(5,20));
+console.log(minimumValue(100,-5));
